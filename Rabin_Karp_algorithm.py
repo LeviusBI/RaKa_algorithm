2adfb02c6c07ecba71461688b6fb198db33fd59a
Leo <82314878+LeviusBI@users.noreply.github.com>
2023-12-16 16:12:37 +0300

    Update Rabin_Karp_algorithm.py

--- Rabin_Karp_algorithm.py
+++ Rabin_Karp_algorithm.py
@@ -4,45 +4,45 @@ import sys
 from Bio import SeqIO
 
 
-def Rabin_Karp(pattern, text, q, d):
+def Rabin_Karp(pattern, text, q, d=10):
     m = len(pattern)
     n = len(text)
     if m > n:
-        raise PatternLength("Pattern P is longer than text T. Insert shorter pattern")
-    p = 0
-    t = 0
-    h = 1
-    i = 0
-    j = 0
-    end = 0
-    
-    for i in range(m-1):
-        h = (h*d) % q
+      print("Pattern P is grater than text T")
+      exit(1)
+    else:
+      p = 0
+      t = 0
+      h = 1
+      i = 0
+      j = 0
+      end = 0
+      for i in range(m-1):
+         h = (h*d) % q
 
-    # Calculate hash value for pattern and text
-    for i in range(m):
-        p = (d*p + ord(pattern[i])) % q
-        t = (d*t + ord(text[i])) % q
+     # Calculate hash value for pattern and text
+      for i in range(m):
+         p = (d*p + ord(pattern[i])) % q
+         t = (d*t + ord(text[i])) % q
 
-    # Find the match
-    for i in range(n-m+1):
-        if p == t:
-            for j in range(m):
-                if text[i+j] != pattern[j]:
-                    break
+     # Find the match
+      for i in range(n-m+1):
+          if p == t:
+              for j in range(m):
+                  if text[i+j] != pattern[j]:
+                      break
 
-            j += 1
-            if j == m:
-                print(str(i))
-                end+=1
+              j += 1
+              if j == m:
+                 print(str(i))
+                 end+=1
+              if i < n-m:
+                  t = (d*(t-ord(text[i])*h) + ord(text[i+m])) % q
 
-        if i < n-m:
-            t = (d*(t-ord(text[i])*h) + ord(text[i+m])) % q
-
-            if t < 0:
-                t = t+q
-    if end==0:
-        print("No matches")
+              if t < 0:
+                 t = t+q
+      if end==0:
+         print("No matches")
 
 text, pattern = sys.argv[1], sys.argv[2]
 
